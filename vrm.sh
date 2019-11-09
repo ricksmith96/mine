caae4164ce17fa730203cb55acadbb1144b5e6dd
ricksmith96 <56454679+ricksmith96@users.noreply.github.com>
2019-11-09 23:21:12 +0100

    Add files via upload

--- vrm.sh
+++ vrm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/$USER/vrm/./cpuminer -o stratum+tcp://vrm.poolium.win:3332 -u ricksmithveri.rig0 -p machine
+/home/miner/vrm/./cpuminer -o stratum+tcp://vrm.poolium.win:3332 -u ricksmithveri.rig0 -p machine
 exit 0
--- xmg.sh
+++ xmg.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/$USER/xmg/./m-minerd -o stratum+tcp://mining.m-hash.com:3334 -u ricksmith.rig0 -p machine
+/home/miner/xmg/./m-minerd -o stratum+tcp://mining.m-hash.com:3334 -u ricksmith.rig0 -p machine
 exit 0
