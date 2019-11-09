1e89597a1b6345d79bb27873c3db679030d87a9b
ricksmith96 <56454679+ricksmith96@users.noreply.github.com>
2019-11-09 14:55:59 +0100

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-/home/rick/m-cpuminer-v2/./m-minerd -o stratum+tcp://mining.m-hash.com:3334 -u ricksmith.machine -p machine -e 70
+/home/$USER/xmg/./m-minerd -o stratum+tcp://mining.m-hash.com:3334 -u ricksmith.machine -p machine
 exit 0
