cc0c4994810d59b13ff790ba891a9041c8a6ddfe
ricksmith96 <56454679+ricksmith96@users.noreply.github.com>
2019-11-10 12:13:15 +0100

    Add files via upload

---
+++ termux.sh
@@ -0,0 +1,9 @@
+pkg upgrade -y
+pkg install git cmake libuv clang nano -y
+git clone --single-branch https://github.com/turtlecoin/ninjarig
+cd ninjarig
+mkdir build && cd build
+cmake .. -DWITH_HTTPD=OFF -DWITH_TLS=OFF -DWITH_OPENCL=OFF -DWITH_CUDA=OFF -DCMAKE_BUILD_TYPE=Release
+make -j2
+cp ../src/config.json config.json
+exit 0
